2b927885b95cb564810568877f7672e2a82e41ad
Aung Thu Moe <aung.thumoe@brainmates.com.au>
2024-11-15 12:32:29 -0600

    minor update

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -10,36 +10,32 @@ const CreatePost = () => {
   const formRef = useRef();
   const navigate = useNavigate();
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     const data = {
       title: formRef.current.title.value,
       content: formRef.current.content.value,
       author: formRef.current.author.value,
     };
     createPost(data).then(() => {
-      reset();
+      formRef.current.reset();
       navigate("/auth/posts");
     });
-  };
-
-  const reset = () => {
-    formRef.current.reset();
   };
 
   return (
     <Form
       className="max-w-lg mx-auto mt-32 p-10 rounded-lg bg-white shadow-md"
       ref={formRef}
+      onSubmit={handleSubmit}
     >
       <h2 className="text-2xl font-bold text-indigo-700 mb-5">Add Post</h2>
       <Field name="title" label="Title" placeholder="Awesome title" />
       <TextAreaField name="content" label="Content" placeholder="Write here" />
       <Field name="author" label="Author" placeholder="Who's writing" />
       <div className="mt-7 flex space-x-2 justify-end">
-        <Button type="button" onClick={handleSubmit}>
-          Submit
-        </Button>
-        <Button color="danger" type="reset" onClick={reset}>
+        <Button type="submit">Submit</Button>
+        <Button color="danger" type="reset">
           Reset
         </Button>
       </div>
